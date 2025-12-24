de0263fb2337d8b0489cf59befa98994af1e6312
M Andriansyah Nurcahya <4andriansyah@gmail.com>
2025-12-24 16:16:08 +0700

    finalize sequelize

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'birthday_wishes',
   process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '',
+  process.env.DB_PASSWORD || 'Password',
   {
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT || '3306'),
